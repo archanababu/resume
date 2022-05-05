1be5c707baa5d8a462dd6ddc191232f6be9ab224
arcbabu <archana.masterseducation@gmail.com>
2022-05-05 16:59:40 -0700

    changes

--- forms/contact.php
+++ forms/contact.php
@@ -8,7 +8,7 @@
 
   // Replace contact@example.com with your real receiving email address
   
-    if(isset($_POST['submit'])){
+    
     $to = "ms.archanababu@gmail.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $from_name = $_POST['name'];
@@ -24,7 +24,7 @@
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
     // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+    
 
 
     /*
