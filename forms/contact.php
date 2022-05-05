1b618e28f83d62db026a8cd50d2f2c0640867267
arcbabu <archana.masterseducation@gmail.com>
2022-05-05 16:55:44 -0700

    contact php

--- forms/contact.php
+++ forms/contact.php
@@ -8,6 +8,26 @@
 
   // Replace contact@example.com with your real receiving email address
   
+    if(isset($_POST['submit'])){
+    $to = "ms.archanababu@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $from_name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = ( $_POST['message'], 'Message', 10);
+    $subject2 = "Copy of your form submission" . $subject;
+    $message = $from_name . " wrote the following:" . "\n\n" . $message;
+    $message2 = "Here is a copy of your message " . $from_name . "\n\n" . $message;
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+
+
+    /*
   $receiving_email_address = 'ms.archanababu@gmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
@@ -34,10 +54,12 @@
   );
   */
 
+  /*
   $contact->add_message( $_POST['name'], 'From');
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send(); 
+  */
   
 ?>
