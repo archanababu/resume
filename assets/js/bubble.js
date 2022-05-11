19076e30a4e94192d78f535dc138411dd734bf6d
arcbabu <archana.masterseducation@gmail.com>
2022-05-10 18:12:02 -0700

    bubble.js script update

--- assets/js/bubble.js
+++ assets/js/bubble.js
@@ -1,29 +1,29 @@
-function myFunction(x){
-    if(x.matches){
-        var layer1 = document.getElementById('bottom-cloud')
-    scroll = window.pageYOffset;
-    document.addEventListener('scroll', function(e){
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer1.style.left = (3 + scroll/30) + '%';
-    });
+(function myFunction(){
+    // if(x.matches){
+    //     var layer1 = document.getElementById('bottom-cloud')
+    // scroll = window.pageYOffset;
+    // document.addEventListener('scroll', function(e){
+    //     var offset = window.pageYOffset;
+    //     scroll = offset;
+    //     layer1.style.left = (3 + scroll/30) + '%';
+    // });
 
-        var layer2 = document.getElementById('top-cloud')
-    scroll = window.pageYOffset;
-    document.addEventListener('scroll', function(e){
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer2.style.left = (70 +scroll/30) +'%';
+    //     var layer2 = document.getElementById('top-cloud')
+    // scroll = window.pageYOffset;
+    // document.addEventListener('scroll', function(e){
+    //     var offset = window.pageYOffset;
+    //     scroll = offset;
+    //     layer2.style.left = (70 +scroll/30) +'%';
 
-    });
-    var layer3 = document.getElementById('mountain')
-    scroll = window.pageYOffset;
-    document.addEventListener('scroll', function(e){
-        var offset = window.pageYOffset;
-        scroll = offset;
-        layer3.style.top = (55 + scroll/30) + '%';
-    });
-    }
+    // });
+    // var layer3 = document.getElementById('mountain')
+    // scroll = window.pageYOffset;
+    // document.addEventListener('scroll', function(e){
+    //     var offset = window.pageYOffset;
+    //     scroll = offset;
+    //     layer3.style.top = (55 + scroll/30) + '%';
+    // });
+    // }
     else{
     var layer1 = document.getElementById('bottom-cloud')
     scroll = window.pageYOffset;
@@ -50,9 +50,9 @@ function myFunction(x){
         layer3.style.top = (41 + scroll/10) + '%';
     });
     }
-}
-if (matchMedia) {
-    const x = window.matchMedia("(max-width: 768px)");
-    x.addListener(myFunction);
-    myFunction(x);
-}
+})()
+// if (matchMedia) {
+//     const x = window.matchMedia("(max-width: 768px)");
+//     x.addListener(myFunction);
+//     myFunction(x);
+// }
